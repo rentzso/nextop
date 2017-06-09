8a105cad06f023f0c33a4b759821fcbfdbd02d5d
Renzo Frigato <rfrigato@stanford.edu>
2017-06-09 00:40:53 -0700

    change user interface

--- src/main.py
+++ src/main.py
@@ -2,24 +2,28 @@ import sys
 import elasticsearch
 import db
 
-
-
 def main():
     start_document = db.getRandomNews()
-    print start_document['url'] + '\n'
+    print start_document['url']
+    print 'Topics: ', start_document['topics'], '\n'
     topics = set(start_document['topics'])
     while True:
         recommendations = db.getRecommendations(topics)
         for i, r in enumerate(recommendations):
-            print i + 1, r['url']
+            print i + 1, ' -> ', r['url']
+        print
         selected = raw_input('select a news article(0 to exit): ')
+        print
         try:
             selected = int(selected) - 1
         except:
             selected = -1
         if selected == -1:
             break
-        topics = topics.union(recommendations[selected])
+        new_topics = recommendations[selected]['topics']
+        print recommendations[selected]['url']
+        print 'Topics: ', new_topics, '\n'
+        topics = topics.union(new_topics)
 
 if __name__ == '__main__':
     main()
