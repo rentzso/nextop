bdfffe67db7c84bb4e1b629abf3935f6317f9769
Renzo Frigato <rfrigato@stanford.edu>
2017-06-20 12:10:55 -0700

    add flask API

---
+++ api/app/views.py
@@ -0,0 +1,54 @@
+from flask import request
+from flask import json
+from app import app
+from elasticsearch import Elasticsearch
+import os
+
+host = 'http://{}:{}@ip-10-0-0-10:9200/'.format(os.environ['ELASTIC_USER'], os.environ['ELASTIC_PASS'])
+es = Elasticsearch([host], timeout=90)
+
+@app.route('/topics', methods=['POST'])
+def get_recommendations():
+    data = request.get_json()
+    topics = data['topics']
+    simple = data.get('simple', True)
+    if simple:
+        return json.dumps(query_simple(topics))
+    else:
+        return json.dumps(query_custom(topics))
+
+def query_custom(topics):
+    should_clause = [
+        {'constant_score': {
+            'filter' : {
+                'match': { 'topics' :  topic} }}}
+        for topic in topics
+    ]
+    query = {
+        'query': {
+            'function_score': {
+                'query': {
+                    'bool': {'should': should_clause }
+                },
+                'script_score' : {
+                    'script' : {
+                        'inline': '- Math.abs(_score/doc[\'num_topics\'].value - 0.75)'
+                    }
+                }
+            }
+        }
+    }
+    return _exec_query(query)
+
+def query_simple(topics):
+    query = {
+        'query': {
+            'match': {'topics': ' '.join(topics)}
+        }
+    }
+    return _exec_query(query)
+
+def _exec_query(query):
+    results = es.search(index='documents', body=query)
+    app.logger.info('a simple request took {} milliseconds'.format(results['took']))
+    return [result['_source'] for result in results['hits']['hits']]
